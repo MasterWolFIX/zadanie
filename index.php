3cd0d45e64d247b85bf23b545270e004b7164e32
Dainshie <you@example.com>
2024-10-25 10:21:43 +0200

    tak

--- index.php
+++ index.php
@@ -3,36 +3,74 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Exec</title>
 </head>
 <body>
+
 <?php
-        $wyjscie = [];
-        $return = 0;
-        exec('net use', $wyjscie, $return);
-        //echo "return {$return}";
-        $mapowany = false;
 
+function getDirectorySize($dir) {
+    $size = 0;
 
-        foreach($wyjscie as $item){
-            if(strpos($item, '10.40.50.2')!== false){
-                echo "Dysk jest zmapowany <a href='?akcja=usun'> usuń</a>";
-                $mapowany = true;
+   
+    if (is_dir($dir)) {
+        
+        $files = scandir($dir);
+        foreach ($files as $file) {
+          
+            if ($file != '.' && $file != '..') {
+                $path = $dir . DIRECTORY_SEPARATOR . $file;
+
+                
+                if (is_dir($path)) {
+                    $size += getDirectorySize($path);
+                } else {
+                    $size += filesize($path);
+                }
             }
         }
-        if(!$mapowany){
-            echo "Dysk jest zmapowany <a href='?akcja=mapuj'>mapuj</a>";
-        }
+    }
+    return $size;
+}
 
-        if(@$_GET['akcja']=='mapuj'){
-            exec('net use z: \\\\10.40.50.2\\pliki /user:uczen uczenpti', $wyjscie, $return);
-            header("location: ./");
+    $wyjscie = [];
+    $rerurn = 0;
+    exec('net use', $wyjscie, $rerurn);
+    $mapowany = false;
+
+
+
+
+    //echo "return {$return}";
+    foreach($wyjscie as $item){
+        if(strpos($item, '10.40.50.2')!==false)
+        {
+            echo 'Dysk jest zmapowany <a href="?akcja=usun">Odmapuj</a>';
+            $mapowany = true;
+
+            $directoryPath = 'Z:\\'; 
+        $sizeInGB = getDirectorySize($directoryPath);
+        echo "Rozmiar zmapowanego katalogu: " . round($sizeInGB, 2);
         }
+    }
         
-        if(@$_GET['akcja']=='usun'){
-            exec('net use Z: /delete /y', $wyjscie, $return);
-            header("location: ./");
-        }
-    ?>
+    if(!$mapowany){
+        echo 'Mapuj <a href="?akcja=mapuj">dysk</a>';
+    }
+
+    if(@$_GET['akcja']=='mapuj'){
+        exec('net use Z: \\\\10.40.50.2\\pliki /user:uczen uczenpti', $wyjscie, $rerurn);
+        header('Location: ./');
+
+    }
+    if(@$_GET['akcja']=='usun'){
+        exec('net use Z: /delete', $wyjscie, $rerurn);
+        header('Location: ./');
+
+    }
+    //print_r($wyjscie);
+
+?>
+    
 </body>
 </html>
